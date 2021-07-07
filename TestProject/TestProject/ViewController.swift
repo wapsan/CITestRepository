e61f7a8e4f95e7decc68b584121e61a475217787
Viacheslav Chupita <vyacheslav.chupita@cogniteq.com>
2021-07-07 14:22:22 +0300

    Second

--- TestProject/TestProject/ViewController.swift
+++ TestProject/TestProject/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         //Changes for push #1
+        //#2
         // Do any additional setup after loading the view.
     }
 
